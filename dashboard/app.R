68ddddc129a907b668969755b7176cbd5d989413
Hanne Oberman <h.i.oberman@uu.nl>
2025-06-23 17:35:16 +0200

    re-work with bar chart

--- dashboard/app.R
+++ dashboard/app.R
@@ -18,101 +18,173 @@ library(ggplot2)
 # dataset
 gs4_deauth()
 sheet_id <- "https://docs.google.com/spreadsheets/d/1JuRPvEEaBwA2nWU8ptUF7Uq8jrtwlt3etAj5PvRcT40/"
-dat_raw <- read_sheet(sheet_id, skip = 1, col_types = "c")
-cols_bin <- apply(dat_raw, 2, unique) |> 
-    sapply(function(x) length(x) <= 3) |> 
-    which() |> 
-    names()
-dat_mut <- dat_raw |> 
-    mutate(
-      `Start data collection` = as.numeric(`Start data collection`),
-      `End data collection` = as.numeric(`End data collection`)
-    ) |>
-    mutate(across(all_of(cols_bin), ~case_when(. == 1 ~ "Included in survey" , . == 0 ~ "Not included")))
-dat_mut <- dat_mut[, c(1, 6:(ncol(dat_mut)-1), 2:5, ncol(dat_mut))]
+dat_raw <- read_sheet(sheet_id, skip = 1, col_types = "c", trim_ws = TRUE)
+# cols_bin <- apply(dat_raw, 2, unique) |>
+#   sapply(function(x)
+#     length(x) <= 2) |>
+#   which() |>
+#   names()
+OS_practices <- c(
+  "Open access",
+  "Open peer-review",
+  "Pre-printing",
+  "Open data",
+  "Open code",
+  "OER",
+  "Pre-registration",
+  "Public engagement"
+)
+dat_mut <- dat_raw |>
+  mutate(
+    `Start data collection` = as.numeric(`Start data collection`),
+    `End data collection` = as.numeric(`End data collection`)
+  ) |>
+  mutate(across(
+    all_of(OS_practices), as.numeric#~case_when(. == 1 ~ "Included in survey" , . == 0 ~ "Not included")
+  ))
+# dat_mut <- dat_mut[, c(1, 6:(ncol(dat_mut) - 1), 2:5, ncol(dat_mut))]
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Open Science Surveys"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            selectInput("o_access", "Open Access", choices = c("Included in survey", "Not included", "No filter"), selected = "No filter"),    
-            selectInput("o_preprint", "Pre-printing", choices = c("Included in survey", "Not included", "No filter"), selected = "No filter"),
-            selectInput("o_peer", "Open Peer-review", choices = c("Included in survey", "Not included", "No filter"), selected = "No filter"),
-            selectInput("o_data", "Open Data", choices = c("Included in survey", "Not included", "No filter"), selected = "No filter"),
-            selectInput("o_code", "Open Code", choices = c("Included in survey", "Not included", "No filter"), selected = "No filter"),
-            selectInput("o_prereg", "Pre-registration", choices = c("Included in survey", "Not included", "No filter"), selected = "No filter"),
-            selectInput("o_er", "Open Educational Resources", choices = c("Included in survey", "Not included", "No filter"), selected = "No filter"),
-            selectInput("o_pe", "Public Engagement", choices = c("Included in survey", "Not included", "No filter"), selected = "No filter"),
-            
-            sliderInput("year",
-                        "Year conducted:",
-                        min = 2010,
-                        max = 2025,
-                        value = c(2018, 2024),
-                        step = 1, 
-                        sep = NULL,
-                        round = TRUE)
-            ),
-        # Show a plot of the generated distribution
-        mainPanel(
-          DTOutput("table"),
-          plotOutput("plot")
-        )
-    )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-  # filtered data
-  filtered <- reactive({dat_out <- dat_mut |> filter(
-    (is.na(`Start data collection`) | `Start data collection` >= input$year[1]) & 
-      (is.na(`End data collection`) | `End data collection` <= input$year[2]))
+  # Application title
+  titlePanel("Open Science Surveys"),
   
-  if (input$o_access != "No filter") {
-    dat_out <- filter(dat_out, `Open access` == input$o_access)
-  }
-  if (input$o_preprint != "No filter") {
-    dat_out <- filter(dat_out, `Pre-printing` == input$o_preprint)
-  }
-  if (input$o_peer != "No filter") {   
-    dat_out <- filter(dat_out, `Open peer-review` == input$o_peer)
-  }
-  if (input$o_data != "No filter") {
-    dat_out <- filter(dat_out, `Open data` == input$o_data)
-  }
-  if (input$o_code != "No filter") {
-    dat_out <- filter(dat_out, `Open code` == input$o_code)
-  }
-  if (input$o_prereg != "No filter") {
-    dat_out <- filter(dat_out, `Pre-registration` == input$o_prereg)
-  }
-  if (input$o_er != "No filter") {
-    dat_out <- filter(dat_out, `OER` == input$o_er)
-  }
-  if (input$o_pe != "No filter") {
-    dat_out <- filter(dat_out, `Public engagement` == input$o_pe)
-  }
-  return(dat_out)
-  })
-  # table
-    output$table <- renderDT({
-        select(filtered(), !all_of(cols_bin))
-        }, options = list(
-          pageLength = 20), rownames = FALSE)
-    # graph
-    output$plot <- renderPlot({
-    dat_long <- tidyr::pivot_longer(filtered(), cols = cols_bin) #select(dat_out, all_of(cols_bin))
-    ggplot(dat_long, aes(y = ifelse(value == "Included in survey", 1, 0), x = name, color = name)) + 
-      geom_jitter(height = 0) +
-      labs(x = "Open Science Practice", y = "Included in survey", title = "Open Science Practices in Surveys") + 
-      theme_classic()
-    })
-    }
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      h4("Studies"),
+      sliderInput(
+        "year",
+        "Year conducted:",
+        min = 2010,
+        max = 2025,
+        value = c(2010, 2025),
+        step = 1,
+        sep = NULL,
+        round = TRUE
+      ),
+      radioButtons(
+        "discipline",
+        "Academic discipline",
+        choices = c("Any", "Social sciences only", "STEM only"),
+        selected = c("Any")
+      ),
+      radioButtons(
+        "region",
+        "Region",
+        choices = c("Any", "EU/UK only", "USA only"),
+        selected = c("Any")
+      ),
+      radioButtons(
+        "target",
+        "Target population",
+        choices = c("Any", "Academic only", "ECR only"),
+        selected = c("Any")
+      ),
+      # checkboxGroupInput(
+      #   "practices",
+      #   "Open Science practices",
+      #   choices = c(
+      #     "Open Access",
+      #     "Pre-printing",
+      #     "Open Peer-review",
+      #     "Open Data",
+      #     "Open Code",
+      #     "Pre-registration",
+      #     "Open Educational Resources",
+      #     "Public Engagement"
+      #   ),
+      #   selected = c(
+      #     "Open Access",
+      #     "Pre-printing",
+      #     "Open Peer-review",
+      #     "Open Data",
+      #     "Open Code",
+      #     "Pre-registration",
+      #     "Open Educational Resources",
+      #     "Public Engagement")
+      #   )
+      ),
+        
+          # Show a plot of the generated distribution
+       mainPanel(
+            tabsetPanel(
+              tabPanel("Summary",
+                "Summary",
+                plotOutput("plot")
+                ),
+              tabPanel("Study details",
+                "Studies",
+                DTOutput("table"))
+              )
+          )
+      )
+)
+      
+      # Define server logic required to draw a histogram
+      server <- function(input, output) {
+        # filtered data
+        filtered <- reactive({
+          dat_out <- dat_mut |> filter(
+            # year
+            (is.na(`Start data collection`) |
+                `Start data collection` >= input$year[1]
+            ) & (is.na(`End data collection`) |
+                  `End data collection` <= input$year[2]
+              )) 
+          
+          # # %>% 
+          #     input$region == "Any" |
+          #       `Region` %in% input$region ifelse(input$discipline == "Any", ., filter(., discipline_cat == input$discipline))
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+          # if (input$discipline != "Any") {
+          #   dat_out <- filter(dat_out, discipline_cat == input$discipline)
+          #   # region_cat == input$region,
+          #   # target_cat == input$target
+          #   
+          # }
+            
+        })
+        
+        # table
+        output$table <- renderDT({
+          select(filtered(), !all_of(cols_bin))
+        }, options = list(pageLength = 20), rownames = FALSE)
+        # graph
+        output$plot <- renderPlot({
+          dat_props <- data.frame(practice = OS_practices, prop = colMeans(filtered()[, OS_practices]) * 100)
+          dat_props <- sort_by(dat_props, dat_props$prop) 
+          dat_props$ordered <- factor(dat_props$practice, levels = dat_props$practice, ordered = TRUE)
+          
+          ggplot(dat_props, aes(y = ordered, x = prop, fill = practice)) +
+            geom_bar(stat = "identity") +
+            labs(x = "Inclusion rate (%)", y = "Open Science practice", fill = NULL) +
+            theme_minimal()
+           
+          # dat_long <- dat_mut |># dat_long <- dat_mut |>NULL
+          #   tidyr::pivot_longer(cols = all_of(OS_practices), names_to = "OS_practice", values_to = "practice_included", values_transform = as.numeric)
+          # props <- dat_long |> group_by(OS_practice) |> summarise(m = mean(practice_included))
+          # order <- sort_by(props, props$m)$OS_practice
+          # dat_long <- mutate(dat_long,
+          #                    OS_practice = factor(OS_practice, levels = order, ordered = TRUE))
+          # ggplot(dat_long, aes(x = practice_included, y = OS_practice, fill = OS_practice)) +
+          #   geom_jitter() # +
+          
+          
+            # scale_y_continuous(labels=percent_format())
+          # 
+          # dat_long <- tidyr::pivot_longer(filtered(), cols = cols_bin) #select(dat_out, all_of(cols_bin))
+          # ggplot(dat_long, aes(
+          #   y = ifelse(value == "Included in survey", 1, 0),
+          #   x = name,
+          #   color = name
+          # )) +
+          #   geom_jitter(height = 0) +
+          #   labs(x = "Open Science Practice", y = "Included in survey", title = "Open Science Practices in Surveys") +
+          #   theme_classic()
+        })
+      }
+      
+      # Run the application
+      shinyApp(ui = ui, server = server)
+      
